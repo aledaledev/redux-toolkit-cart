2582bb71a04f4e8b3803fd372727ee8a07681170
aledale <ale.salas271102@gmail.com>
2022-12-29 21:05:23 -0300

    playing with thunkAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { store } from './app/store'
 import styles from './assets/styles/App.module.css'
 import List from './components/CartContainer'
 import Modal from './components/Modal'
 import Navbar from './components/Navbar'
 import { useModal } from './context/ModalContext'
+import { getCartItems, setTotals } from './features/cart/cartSlice'
+import { CartState } from './types'
 
 function App() {
 
-  //tambien sirve para no repetir el dispatch donde sea necesario
-  /*const {cartItems} = useSelector(store => store.cart)
+  const {cartItems,isLoading} = useSelector(store => store.cart) as CartState
   const dispatch = useDispatch()
+  const {modal} = useModal()
 
   useEffect(()=>{
     dispatch(setTotals())
-  },[cartItems])*/
+  },[cartItems])
 
-  const {modal} = useModal()
+  useEffect(()=>{
+    dispatch(getCartItems('random'))
+  },[]) 
+
+  if(isLoading){
+    return <div>
+        <h2>Loading...</h2>
+    </div>
+  }
+  
 
   return (
     <main>
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,11 +1,38 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import dataItems from '../../data/dataItems.json'
+//import dataItems from '../../data/dataItems.json'
 import { CartState } from "../../types";
+import axios from 'axios'
+
+//callback function
+//lifecycle actions
+//thunkAPI
+
+const url = "https://course-api.com/react-useReducer-cart-project"
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', 
+/*()=>{
+    return fetch(url)   
+    .then(resp => resp.json())
+    .catch(err => console.log(err))
+}*/
+
+//thunk API is powerful
+ async (name,thunkAPI)=>{
+    try {
+        //console.log(thunkAPI.getState());
+        //thunkAPI.dispatch(cleanCart())
+        const resp = await axios(url)
+        return resp.data
+    } catch (err){
+        return thunkAPI.rejectWithValue('something is wrong')
+    }
+}
+) 
 
 const initialState:CartState = {
-    cartItems: dataItems,
+    cartItems: [],
     amount: 4,
-    total: Number((dataItems.map(item=> Number(item.price)*item.amount).reduce((prev,cur) => prev+cur)).toFixed(2)),
+    total: 0,
     isLoading: true
 }
 
@@ -42,6 +69,20 @@ const cartSlice = createSlice({
                 state.amount=state.cartItems.map(item=> item.amount).reduce((prev,cur) => prev+cur)
                 state.total=Number((state.cartItems.map(item=> Number(item.price)*item.amount).reduce((prev,cur) => prev+cur)).toFixed(2))
             }
+        }
+    },
+    extraReducers:{
+        [getCartItems.pending]:(state) =>{
+            state.isLoading = true
+        },
+        [getCartItems.fulfilled]:(state,action) =>{
+            console.log(action);
+            state.isLoading = false
+            state.cartItems= action.payload
+        },
+        [getCartItems.rejected]:(state,action) => {
+            console.log(action);
+            state.isLoading= false
         }
     }
 })
